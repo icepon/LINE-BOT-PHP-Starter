d3772028564c5bf2b01eb87c6ce23bbf844fb6e3
icepon <ice_pon@hotmail.com>
2017-09-22 23:02:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,7 +30,13 @@ $replyToken = $event['replyToken'];
 
 if ($text == "สวัสดี"){
 //$messages = [ 'type'=>'text','text'=>"สวัสดีจ้าาาา"]; }
-$messages = [ 'type'=>'template','template'=>['type'=>'confirm','text'=>"sure"]]; }
+$messages = [ 'type'=>'template',
+             'template'=>['type'=>'confirm','text'=>"sure",
+                          'action'=>['type': 'message',
+            'label': "Yes",
+            'text': "yes"]
+                         
+                         ]]; }
 else if ($text == "ทำไรอยู่") {
 $messages = [ 'type'=>'text','text'=>"ไม่บอก อิอิ"]; }
 else if ($text == "ไอซ์") {
