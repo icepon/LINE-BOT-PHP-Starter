9f218fc1f0fadfce6decd0462d4fa67295381d91
icepon <ice_pon@hotmail.com>
2017-09-22 13:45:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,14 @@
 <?php
 $access_token = 'AdlGlZFCmua0+/PYr/y/iI7dF2c7DeVXkhG/FKp9K4Pp8qAuEWTv7yAx7vDX1t3B31gVTyIwIMhyO4g1XWptfVyFJ7kmUGdDrfB4Pd/UspZp0iIMrLeVq+YJIV0ZY0arNIDv4eVmwLOCm2yns5ezewdB04t89/1O/w1cDnyilFU=';
-
+$access_secret = '1efe3c53bbd84320ce616b832c262094';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV[$access_token]);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV[$access_secret]]);
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
