262ddaedb032f3e6880623163745228e5af4b7c8
icepon <ice_pon@hotmail.com>
2017-09-22 15:58:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				
-				'type' => 'template', 'template'=>{'type'=>'buttons','text'=>'what','action'=>[{'type'=>'uri','label'=>'google','uri'=>'www.google.com'}]}
+				'type' => 'template', 'template'=>['type'=>'buttons','text'=>'what','action'=>[['type'=>'uri','label'=>'google','uri'=>'www.google.com']]]
 				
 				
 				
